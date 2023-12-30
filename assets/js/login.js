b3bc5950be2a12cab879181ca3c1fa416a1981c2
Anderson Padoan <andy_paduano@hotmail.com>
2023-12-30 12:20:44 -0300

    add memory game pages and add alert with game rule

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,9 +2,9 @@ const input = document.querySelector(".login_input");
 const button = document.querySelector(".login_button");
 const form = document.querySelector(".login_page");
 
-
+// function to validate input //
 function inputValidator(event) {
- if(event.target.value.length > 3) {
+ if(event.target.value.length > 2) {
     button.removeAttribute('disabled');
  } else {
     button.setAttribute('disabled', '');
@@ -16,9 +16,11 @@ function inputValidator(event) {
 function submitValidator(event) {
     event.preventDefault();
     
+    alert("Match pairs of corresponding cards by flipping two at a time, remembering their positions.");
     localStorage.setItem('name', input.value);
     window.location = 'assets/memorygame.html';
 }
+
 
 
 input.addEventListener('input', inputValidator);
