4fc0a41e894330fe38533f094c5b21c0f600fb8e
Anderson Padoan <andy_paduano@hotmail.com>
2024-01-07 10:39:14 -0300

    test media screen 760 px

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,13 @@
 const input = document.querySelector(".login_input");
 const button = document.querySelector(".login_button");
 const form = document.querySelector(".login_page");
-//const memoryGameRules = [
+const memoryGameRules = [
       "1. Find pairs of matching cards to earn points.",
       "2. Choose two face-down cards in each turn.",
       "3. If the cards make a pair, they are removed from the game.",
       "4. Otherwise, the cards are flipped face down again.",
       "5. The game ends when all pairs are found; the player with the most pairs wins."
-    ];//
+    ];
  
 
 // function to validate input //
@@ -24,7 +24,7 @@ function inputValidator(event) {
 function submitValidator(event) {
     event.preventDefault();
     
-   // alert(memoryGameRules);//
+    alert(memoryGameRules)
     localStorage.setItem('name', input.value);
     window.location = 'assets/memorygame.html';
 }
